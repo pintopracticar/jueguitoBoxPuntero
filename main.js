6cd4e31ab16e9c2241f7f427b21476b52df5518b
pintopracticar <pintopracticar@users.noreply.github.com>
2015-03-27 15:50:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
     var punteroGrafico = document.getElementById("punterografico"), //Puntero div que se movera con los controles
-    i; 
+    i,
+    intervalo,
+    alturaCheck,
+    anchuraCheck,
+    totalMorfadas = 0,
+    morfed = false; 
     
     // Hovers especificos para cada posición
     $(".arriba").hover(function(){        
-        $(".arriba").css("background-position-x", "85px")
+        $(".arriba").css("background-position-x", "85px");
     },
     function(){        
-        $(".arriba").css("background-position-x", "0")
+        $(".arriba").css("background-position-x", "0");
     });
     $(".abajo").hover(function(){        
-        $(".abajo").css("background-position-x", "85px")
+        $(".abajo").css("background-position-x", "85px");
     },
     function(){        
-        $(".abajo").css("background-position-x", "0")
+        $(".abajo").css("background-position-x", "0");
     });
     $(".derecha").hover(function(){        
-        $(".derecha").css("background-position-x", "85px")
+        $(".derecha").css("background-position-x", "85px");
     },
     function(){        
-        $(".derecha").css("background-position-x", "0")
+        $(".derecha").css("background-position-x", "0");
     });
     $(".izquierda").hover(function(){        
-        $(".izquierda").css("background-position-x", "85px")
+        $(".izquierda").css("background-position-x", "85px");
     },
     function(){        
-        $(".izquierda").css("background-position-x", "0")
+        $(".izquierda").css("background-position-x", "0");
     });
     // Eventos asignados para el click en cada tecla
     $(".arriba").mousedown(function(){
@@ -40,7 +45,38 @@ $(document).ready(function(){
                 temp = temp.toString();
                 $(punteroGrafico).css("top",temp+"px");
             }
-        }, 25);    
+            // Comprobar si alguna de las 10 frutitas fue comida por el puntero
+            
+            // Tomo la distancia del top y le sumo la altura del cuadrado
+            alturaCheck = $(punteroGrafico).css("top");
+            alturaCheck = alturaCheck.replace("px","");
+            alturaCheck = parseInt(alturaCheck);
+            altura = alturaCheck; // esta var llevara solo la dist del top, sin suma
+            alturaCheck = alturaCheck + 50;
+            
+            
+            // Tomo la distancia left y le sumo la anchura del cuadrado
+            anchuraCheck = $(punteroGrafico).css("left");
+            anchuraCheck = anchuraCheck.replace("px","");
+            anchuraCheck = parseInt(anchuraCheck);
+            anchura = anchuraCheck; // esta var llevara solo la dist del left, sin suma
+            anchuraCheck = anchuraCheck + 50;
+            
+            // Comprobaremos la altura y anchura de todas las frutitas para ver si matchean con el puntero y son "morfadas" por dicho puntero
+            for(i=0;i<10;i++){
+                if (altura < frutitas[i].topDist && $(".frutita"+i).css("display") != "none"){
+                    if (alturaCheck > frutitas[i].topDist){
+                        if (anchura < frutitas[i].leftDist){
+                            if (anchuraCheck > frutitas[i].leftDist){
+                                $(".frutita"+i).fadeOut(0);
+                                totalMorfadas++;
+                                $(".morfadas").text(totalMorfadas);
+                            }
+                        }
+                    }
+                }
+            }
+        }, 10);
     }).mouseup(function(){
         clearInterval(intervalo);
     }).mouseleave(function(){
@@ -56,7 +92,38 @@ $(document).ready(function(){
                 temp = temp.toString();
                 $(punteroGrafico).css("top",temp+"px");
             }
-        }, 25);
+            // Comprobar si alguna de las 10 frutitas fue comida por el puntero
+            
+            // Tomo la distancia del top y le sumo la altura del cuadrado
+            alturaCheck = $(punteroGrafico).css("top");
+            alturaCheck = alturaCheck.replace("px","");
+            alturaCheck = parseInt(alturaCheck);
+            altura = alturaCheck; // esta var llevara solo la dist del top, sin suma
+            alturaCheck = alturaCheck + 50;
+            
+            
+            // Tomo la distancia left y le sumo la anchura del cuadrado
+            anchuraCheck = $(punteroGrafico).css("left");
+            anchuraCheck = anchuraCheck.replace("px","");
+            anchuraCheck = parseInt(anchuraCheck);
+            anchura = anchuraCheck; // esta var llevara solo la dist del left, sin suma
+            anchuraCheck = anchuraCheck + 50;
+            
+            // Comprobaremos la altura y anchura de todas las frutitas para ver si matchean con el puntero y son "morfadas" por dicho puntero
+            for(i=0;i<10;i++){
+                if (altura < frutitas[i].topDist && $(".frutita"+i).css("display") != "none"){
+                    if (alturaCheck > frutitas[i].topDist){
+                        if (anchura < frutitas[i].leftDist){
+                            if (anchuraCheck > frutitas[i].leftDist){
+                                $(".frutita"+i).fadeOut(0);
+                                totalMorfadas++;
+                                $(".morfadas").text(totalMorfadas);
+                            }
+                        }
+                    }
+                }
+            }
+        }, 10);
     }).mouseup(function(){
         clearInterval(intervalo);
     }).mouseleave(function(){
@@ -72,7 +139,38 @@ $(document).ready(function(){
                 temp = temp.toString();
                 $(punteroGrafico).css("left",temp+"px");
             }
-        }, 25);
+            // Comprobar si alguna de las 10 frutitas fue comida por el puntero
+            
+            // Tomo la distancia del top y le sumo la altura del cuadrado
+            alturaCheck = $(punteroGrafico).css("top");
+            alturaCheck = alturaCheck.replace("px","");
+            alturaCheck = parseInt(alturaCheck);
+            altura = alturaCheck; // esta var llevara solo la dist del top, sin suma
+            alturaCheck = alturaCheck + 50;
+            
+            
+            // Tomo la distancia left y le sumo la anchura del cuadrado
+            anchuraCheck = $(punteroGrafico).css("left");
+            anchuraCheck = anchuraCheck.replace("px","");
+            anchuraCheck = parseInt(anchuraCheck);
+            anchura = anchuraCheck; // esta var llevara solo la dist del left, sin suma
+            anchuraCheck = anchuraCheck + 50;
+            
+            // Comprobaremos la altura y anchura de todas las frutitas para ver si matchean con el puntero y son "morfadas" por dicho puntero
+            for(i=0;i<10;i++){
+                if (altura < frutitas[i].topDist && $(".frutita"+i).css("display") != "none"){
+                    if (alturaCheck > frutitas[i].topDist){
+                        if (anchura < frutitas[i].leftDist){
+                            if (anchuraCheck > frutitas[i].leftDist){
+                                $(".frutita"+i).fadeOut(0);
+                                totalMorfadas++;
+                                $(".morfadas").text(totalMorfadas);
+                            }
+                        }
+                    }
+                }
+            }
+        }, 10);
     }).mouseup(function(){
         clearInterval(intervalo);
     }).mouseleave(function(){
@@ -88,7 +186,38 @@ $(document).ready(function(){
                 temp = temp.toString();
                 $(punteroGrafico).css("left",temp+"px");
             }
-        }, 25);
+            // Comprobar si alguna de las 10 frutitas fue comida por el puntero
+            
+            // Tomo la distancia del top y le sumo la altura del cuadrado
+            alturaCheck = $(punteroGrafico).css("top");
+            alturaCheck = alturaCheck.replace("px","");
+            alturaCheck = parseInt(alturaCheck);
+            altura = alturaCheck; // esta var llevara solo la dist del top, sin suma
+            alturaCheck = alturaCheck + 50;
+            
+            
+            // Tomo la distancia left y le sumo la anchura del cuadrado
+            anchuraCheck = $(punteroGrafico).css("left");
+            anchuraCheck = anchuraCheck.replace("px","");
+            anchuraCheck = parseInt(anchuraCheck);
+            anchura = anchuraCheck; // esta var llevara solo la dist del left, sin suma
+            anchuraCheck = anchuraCheck + 50;
+            
+            // Comprobaremos la altura y anchura de todas las frutitas para ver si matchean con el puntero y son "morfadas" por dicho puntero
+            for(i=0;i<10;i++){
+                if (altura < frutitas[i].topDist && $(".frutita"+i).css("display") != "none"){
+                    if (alturaCheck > frutitas[i].topDist){
+                        if (anchura < frutitas[i].leftDist){
+                            if (anchuraCheck > frutitas[i].leftDist){
+                                $(".frutita"+i).fadeOut(0);
+                                totalMorfadas++;
+                                $(".morfadas").text(totalMorfadas);
+                            }
+                        }
+                    }
+                }
+            }
+        }, 10);
     }).mouseup(function(){
         clearInterval(intervalo);
     }).mouseleave(function(){
@@ -101,12 +230,14 @@ $(document).ready(function(){
         segundero = $('.timerseg').text();
         segundero = parseInt(segundero);
         segundero++;
-        if(segundero < 60){ // Mientras se puedan sumar segundos, se suman
-            $('.timerseg').text(segundero);
-        } else { // Caso contrario agregamos un minuto
-            segundero = 0;
-            $('.timerseg').text(segundero);
-            sumaMinuto();
+        if(totalMorfadas < 10){ // Si todavia no se morfaron todas, el reloj funcionara
+            if(segundero < 60){ // Mientras se puedan sumar segundos, se suman
+                $('.timerseg').text(segundero);
+            } else { // Caso contrario agregamos un minuto
+                segundero = 0;
+                $('.timerseg').text(segundero);
+                sumaMinuto();
+            }
         }
     }
     function sumaMinuto(){
@@ -118,17 +249,31 @@ $(document).ready(function(){
     // Ejecucion del timer
     setInterval(sumaSegundo, 1000);
     
+    // Inicializo array que contendra todas las frutitas
+    var frutitas = [];
+    
+    function addFrutita(i,randTop,randLeft){
+        this.id = i;
+        this.topDist = randTop;
+        this.leftDist = randLeft;
+    };
+    
     // Generacion de frutitas en el campo    
     for(i=0;i<10;i++){
-        var randTop = Math.floor((Math.random() * 328) - 60), //Distancia top relative en el css, numero al azar
-        randLeft = Math.floor((Math.random() * 790) + 90); // Distancia del top left relative en el css, al azar tambien
+        var randTop = Math.floor((Math.random() * 420) - 1), //Distancia top relative en el css, numero al azar
+        randLeft = Math.floor((Math.random() * 790) + 90), // Distancia del top left relative en el css, al azar tambien
+        spRandTop, // dist top para pasaje de var en int
+        spRandLeft; // dist top para pasaje de var en int
+        spRandTop = randTop;
+        spRandLeft = randLeft;
         randTop = randTop.toString();
         randTop = randTop+"px";
         randLeft = randLeft.toString();
         randLeft = randLeft+"px";
         $(".frutita"+i).css("top",randTop);
         $(".frutita"+i).css("left",randLeft);
+        var frutiTemp = new addFrutita(i,spRandTop,spRandLeft);
+        frutitas.push(frutiTemp);
     }
-    
     
 });
